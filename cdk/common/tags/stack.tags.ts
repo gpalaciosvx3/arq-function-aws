8f0628a7cc78dff040d206ab318fa7bc9c20f59a
Gustavo Palacios <gustavoalonso1997@hotmail.com>
2026-05-29 14:19:21 -0500

    feat: agregando tags

---
+++ cdk/common/tags/stack.tags.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class StackTags {
+  static apply(scope: Construct): void {
+    cdk.Tags.of(scope).add('Project', 'arq-serverless-cdk');
+    cdk.Tags.of(scope).add('ManagedBy', 'aws-cdk');
+    cdk.Tags.of(scope).add('Repository', 'https://github.com/gpalaciosvx3/arq-serverless-cdk');
+  }
+}
--- cdk/lib/stacks/ping.stack.ts
+++ cdk/lib/stacks/ping.stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { StackTags } from '../../common/tags/stack.tags';
 import { PingFnConstruct } from '../ping-constructs/lambda/ping/ping-fn.construct';
 import { HttpApiConstruct } from '../ping-constructs/api-gateway/http-api.construct';
 
@@ -8,7 +9,12 @@ export class PingStack extends cdk.Stack {
   readonly pingFn: NodejsFunction;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, {
+      ...props,
+      description: 'Arquetipo serverless con AWS CDK - Stack de ejemplo para función Ping',
+    });
+
+    StackTags.apply(this);
 
     const pingFnConstruct = new PingFnConstruct(this, 'PingFn');
     this.pingFn = pingFnConstruct.fn;
